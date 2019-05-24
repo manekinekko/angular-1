d46f84bc6617bd91b6c6e7cfa34a252a3cf02b5e
Tanya0609 <tanyi3025@gmail.com>
2019-05-24 16:00:22 -0700

    web

---
+++ angular_to_be_verify/angular_to_be_verify/src/app/app.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular_to_be_verify';
+}
---
+++ angular_to_be_verify/angular_to_be_verify/src/app/app.module.ts
@@ -0,0 +1,34 @@
+﻿import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/NavBar/navbar.component';
+import { FooterComponent } from './components/Footer/footer.component';
+import {Master_DetailModule} from './components/Master_Detail/Master_Detail.module';
+import {ListModule} from './components/List/List.module';
+import {GridModule} from './components/Grid/Grid.module';
+import {BlankModule} from './components/Blank/Blank.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    Master_DetailModule,
+    ListModule,
+    GridModule,
+    BlankModule,
+    RouterModule.forRoot([
+      { path: '**', redirectTo: 'Blank'}
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
